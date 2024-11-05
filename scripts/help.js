e126b34216d769b495bda017392babfa1069c829
Brian Lo <brian000901@gmail.com>
2024-11-05 09:45:01 +0800

    Update help.js

--- scripts/help.js
+++ scripts/help.js
@@ -2,14 +2,15 @@
 module.exports = async function(message) {
     if (message.content === '!!help') {
         const response = `
-        **可 使 用 的 指 令 列 表 ： **
-        /ping - 看 看 機 器 人 是 不 是 活 的
-        !!help - 查 看 這 個 列 表
-        !status - 查 看 伺 服 器 狀 態
-        **——以 下 指 令 需 要 權 限 ——**
-        !autoupdate - 自 動 更 新 伺 服 器 狀 態
-        !console [指 令 ] - 執 行 RCON指 令
-        !reload - 重 新 載 入 腳 本
+        **可  使  用  的  指  令  列  表  ： **
+        /ping - 看  看  機  器  人  是  不  是  活  的
+        !!help - 查  看  這  個  列  表
+        !status - 查  看  伺  服  器  狀  態
+        !rate [內 容 ] [最 小 值 ] [最 大 值 ] [時 間 上 限 (秒 )] - 評  分  指  令
+        **——以  下  指  令  需  要  權  限  ——**
+        !autoupdate - 自  動  更  新  伺  服  器  狀  態
+        !console [指  令 ] - 執  行  RCON 指  令
+        !reload - 重  新  載  入  腳  本
         `;
         message.channel.send(response);
     }
