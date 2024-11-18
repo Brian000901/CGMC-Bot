6f24cb6eaf421070c191d0f3ff5ea76de0fc8118
Brian <brian000901@gmail.com>
2024-11-18 19:22:04 +0800

    Update help.js

--- scripts/help.js
+++ scripts/help.js
@@ -5,6 +5,7 @@ module.exports = async function(message) {
         **可使用的指令列表：**
         /ping - 看看機器人是不是活的
         !!help - 查查看這個列表
+        !botstatus - 查詢Discord機器人狀態
         !status - 查查看伺服器狀態
         **——以下指令需要權限——**
         !autoupdate - 自動更新伺服器狀態
