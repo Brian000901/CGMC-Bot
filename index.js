e796cf3c23f0242f03069022d6f5b18dfea3fba8
brian000901 <your.email@example.com>
2024-11-22 09:59:50 +0000

    removed os

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
-const os = require('os');
 const axios = require('axios');
 const { Client, GatewayIntentBits, Partials, EmbedBuilder, REST, Routes, AttachmentBuilder } = require('discord.js');
 const winston = require('winston');
