970a79280bf7ac66504d816cdf579a6dbbca11c0
Brian <brian000901@gmail.com>
2025-09-09 14:26:16 +0800

    Update help.js

--- scripts/help.js
+++ scripts/help.js
@@ -4,9 +4,9 @@ module.exports = async function(message) {
         const response = `
         **可使用的指令列表：**
         /ping - 看看機器人是不是活的
-        !!help - 查查看這個列表
+        !!help - 查看這個列表
         !botinfo - 查詢Discord機器人狀態
-        !status - 查查看伺服器狀態
+        !status - 查看伺服器狀態
         **——以下指令需要權限——**
         !autoupdate - 自動更新伺服器狀態
         !console [指令] - 執行RCON指令
